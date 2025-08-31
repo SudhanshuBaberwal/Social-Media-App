0fed15d5e06d5a9e53e81157cd86c840881c00d1
SudhanshuBaberwal <24bcs147@iiitdwd.ac.in>
2025-08-31 21:44:21 +0530

    design connections page

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,6 +2,7 @@ import { Badge, Heart, Link, MessageCircle, Share2 } from "lucide-react";
 import React, { useState } from "react";
 import moment from "moment";
 import { dummyUserData } from "../../public/assets";
+import { useNavigate } from "react-router-dom";
 
 const PostCard = ({ post }) => {
   const postWithhashtags = post.content.replace(
@@ -13,10 +14,15 @@ const PostCard = ({ post }) => {
 
   const handleLike = async () => {};
 
+  const navigate = useNavigate();
+
   return (
     <div className="bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl">
       {/* User Info  */}
-      <div className="inline-flex items-center gap-3 cursor-pointer">
+      <div
+        onClick={() => navigate("/profile/" + post.user._id)}
+        className="inline-flex items-center gap-3 cursor-pointer"
+      >
         <img
           src={post.user.profile_picture}
           className="w-10 h-10 rounded-full shadow"
@@ -67,11 +73,11 @@ const PostCard = ({ post }) => {
           <span>{likes.length}</span>
         </div>
         <div className="flex items-center gap-1">
-                <MessageCircle className="w-4 h-4" />
+          <MessageCircle className="w-4 h-4" />
           <span>{12}</span>
         </div>
         <div className="flex items-center gap-1">
-                <Share2 className="w-4 h-4" />
+          <Share2 className="w-4 h-4" />
           <span>{7}</span>
         </div>
       </div>
